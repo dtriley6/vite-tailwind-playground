1acf9e3ce496ce11603282b39eb7d4a2f74afc6e
David Riley <davidriley@Davids-MacBook-Pro-2.local>
2025-04-08 12:55:25 -0400

    pages and ts format.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
